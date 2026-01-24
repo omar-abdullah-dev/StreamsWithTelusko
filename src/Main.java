2ae38f27d540e81b0bd0ab474410db5d367f79a1
omarAbdullahMoharam <omarmoharam790@gmail.com>
2026-01-24 14:58:58 +0200

    Add .gitignore and implement Java streams in Main class for data processing

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.List;
+
+/*****
+ *                                               the need of streams in java
+ *                                               ============================
+ * 1. Simplified Code: Streams provide a more concise and readable way to process collections of data compared to traditional loops.
+ * 2. Functional Programming: Streams support functional programming paradigms, allowing developers to use lambda expressions for operations like filtering, mapping, and reducing.
+ * 3. Lazy Evaluation: Streams use lazy evaluation, meaning that operations are not executed until a terminal operation is invoked. This can lead to performance improvements by avoiding unnecessary computations.
+ * 4. Parallel Processing: Streams can be easily parallelized, allowing for concurrent processing of data, which can significantly improve performance on large datasets.
+ * 5. Improved Readability: The declarative nature of streams makes the code easier to understand and maintain, as it focuses on what to do with the data rather than how to do it.
+ * 6. Built-in Operations: The Streams API provides a rich set of built-in operations for common data processing tasks, reducing the need for boilerplate code.
+ * 7. Better Integration with Collections: Streams integrate seamlessly with Java Collections, making it easy to convert between collections and streams.
+ * 8. Enhanced Data Manipulation: Streams allow for complex data manipulation tasks to be performed in a more straightforward manner, such as grouping, partitioning, and joining data.
+ */
+
+
+public class Main  {
+	public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList( 4, 5, 6, 71, 2, 3,0, 8, 9, 10);
+//        using normal loop:
+        for (int i =0 ;i<nums.size();i++){
+            if (nums.get(i)%2==0){
+                System.out.println(nums.get(i));
+            }
+        }
+//        using enhanced for loop:
+        for (int num: nums){
+            if (num%2==0){
+                System.out.println(num);
+            }
+        }
+
+//        using forEach method:
+        nums.forEach(num -> {
+            if (num % 2 == 0) {
+                System.out.println(num);
+            }
+        });
+
+//        using streams:
+        nums.stream()
+                .filter(num -> num % 2 == 0)
+                .forEach(System.out::println);
+    }
+}
